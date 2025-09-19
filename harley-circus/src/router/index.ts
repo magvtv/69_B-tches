747edbfc80ef2c78aa51dd1a00fb350c9c443be0
magvtv <magutujr@gmail.com>
2025-09-19 22:25:45 +0300

    perf: optimize the routes to various game levels

--- harley-circus/src/router/index.ts
+++ harley-circus/src/router/index.ts
@@ -8,15 +8,38 @@ const routes: RouteRecordRaw[] = [
   },
   {
     path: '/game',
+    name: 'GameHub',
     component: () => import('@/pages/game/index.vue'),
-    children: [
-      { path: '', name: 'Game', component: () => import('@/pages/Game.vue') },
-      { path: 'level1', name: 'Level1', component: () => import('@/pages/game/Level1.vue') },
-      { path: 'level2', name: 'Level2', component: () => import('@/pages/game/Level2.vue') },
-      { path: 'level3', name: 'Level3', component: () => import('@/pages/game/Level3.vue') },
-      { path: 'level4', name: 'Level4', component: () => import('@/pages/game/Level4.vue') },
-      { path: 'level5', name: 'Level5', component: () => import('@/pages/game/Level5.vue') },
-    ],
+  },
+  {
+    path: '/game/level0',
+    name: 'Level0',
+    component: () => import('@/pages/game/Level0.vue'),
+  },
+  {
+    path: '/game/level1',
+    name: 'Level1',
+    component: () => import('@/pages/game/Level1.vue'),
+  },
+  {
+    path: '/game/level2',
+    name: 'Level2',
+    component: () => import('@/pages/game/Level2.vue'),
+  },
+  {
+    path: '/game/level3',
+    name: 'Level3',
+    component: () => import('@/pages/game/Level3.vue'),
+  },
+  {
+    path: '/game/level4',
+    name: 'Level4',
+    component: () => import('@/pages/game/Level4.vue'),
+  },
+  {
+    path: '/game/level5',
+    name: 'Level5',
+    component: () => import('@/pages/game/Level5.vue'),
   },
 ]
 
